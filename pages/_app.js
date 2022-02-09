8ebf9e946e46ec18f53a8cd0ace8df645962de84
Chimobi Mbah <mbahchiboy2@gmail.com>
2022-02-09 18:44:04 +0100

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,34 @@
 import SSRProvider from "react-bootstrap/SSRProvider";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useRouter } from "next/router";
 
 import { wrapper } from "../redux/store";
 import Layout from "../components/Layout";
 import "../styles/all.scss";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const handleStart = (_) => {
+    NProgress.start();
+  };
+  const handleStop = () => {
+    NProgress.done();
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <SSRProvider>
       <Layout>
